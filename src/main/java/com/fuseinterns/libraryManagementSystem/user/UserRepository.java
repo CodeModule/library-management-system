ee01e7bf72fb7f847ae49ca3df571e33fa663b69
Pratyush Pradhan <pratyushplp@gmail.com>
2018-06-27 16:48:06 +0545

    Merge branch 'master' of https://github.com/CodeModule/library-management-system

--- src/main/java/com/fuseinterns/libraryManagementSystem/user/UserRepository.java
+++ src/main/java/com/fuseinterns/libraryManagementSystem/user/UserRepository.java
@@ -3,6 +3,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<MainUser,String>{
 
-	public User findOneByName(String name);
 }
 
