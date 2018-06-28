191a4d306a1704adf1cec8b82b077b0acffa1349
Uttam Sapkota <usapkota@gmail.com>
2018-06-28 15:55:45 +0545

    Borrow Controller

--- src/main/java/com/fuseinterns/libraryManagementSystem/borrower/BorrowService.java
+++ src/main/java/com/fuseinterns/libraryManagementSystem/borrower/BorrowService.java
@@ -1,5 +1,37 @@
 package com.fuseinterns.libraryManagementSystem.borrower;
 
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fuseinterns.libraryManagementSystem.book.BookRepository;
+
+//import com.fuseinterns.libraryManagementSystem.user.User;
+
+@Service
 public class BorrowService {
+	Scanner sc = new Scanner(System.in);
+	
+	@Autowired
+	BookRepository bookRepository;
+	
+	@Autowired
+	static
+	BorrowRepository borrowRepository;
+	
+
+	
+	public Borrow add(Borrow b){
+		return borrowRepository.save(b);
+	}
+	
+	public boolean receiveBook(int id) {
+		List<Borrow> borrows = borrowRepository.findByBookIdAndReturned(id, 0);
+		Borrow borrow = borrows.get(0);
+		borrowRepository.save(borrow);
+		return true;
+	}
 
 }
