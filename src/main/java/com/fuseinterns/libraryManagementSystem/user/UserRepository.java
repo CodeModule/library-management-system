ff27dab2f37115f4439d2868287479bc946f4d34
ishandongol <ishandongol@gmail.com>
2018-06-28 15:58:58 +0545

    for full

--- src/main/java/com/fuseinterns/libraryManagementSystem/user/UserRepository.java
+++ src/main/java/com/fuseinterns/libraryManagementSystem/user/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User,String>{
 
-	public List<User> findByfirstName(String name);
+	 List<User> findByfirstName(String name);
 }
 
 
